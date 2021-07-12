ac8580649969ee98f6f72ccca6a57d8092a2da89
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-12 03:57:07 -0600

    use env instead of secrets on client side; read square creds

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const SQUARE_CLIENT_ID = process.env.SQUARE_CLIENT_ID;
 
 // these aren't really secrets
-router.get("/api/auth0-secrets", function(req, res) {
+router.get("/api/env", function(req, res) {
   const creds = {
     AUTH0_DOMAIN: AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: AUTH0_CLIENT_ID,
+    SQUARE_CLIENT_ID: SQUARE_CLIENT_ID,
   };
   return res.json(creds);
 });
--- src/main.js
+++ src/main.js
@@ -1,53 +1,50 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-import App from '@/App.vue';
-import router from '@/router';
-import store from '@/store';
-import api from '@/api';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import api from "@/api";
 
 Vue.prototype.$api = api;
+Vue.prototype.env = {};
 
 Vue.prototype.wait = async function(ms) {
   return new Promise((resolve) => setTimeout(() => resolve(), ms));
-}
+};
 
 import { Auth0Plugin } from "./auth";
 
 async function main() {
-
-  let resp = await fetch('/api/auth0-secrets');
-  let secrets = await resp.json();
-  let domain = secrets.AUTH0_DOMAIN;
-  let clientId = secrets.AUTH0_CLIENT_ID;
+  let resp = await api.get("/api/env");
+  let env = resp.data;
+  Vue.prototype.env = env;
 
   Vue.use(Vuetify);
   let vuetify = new Vuetify({});
 
   Vue.use(Auth0Plugin, {
-    domain,
-    clientId,
-    onRedirectCallback: appState => {
+    domain: env.AUTH0_DOMAIN,
+    clientId: env.AUTH0_CLIENT_ID,
+    onRedirectCallback: (appState) => {
       router.push(
         appState && appState.targetUrl
           ? appState.targetUrl
           : window.location.pathname
       );
-    }
+    },
   });
-  
+
   Vue.config.productionTip = false;
-  
+
   new Vue({
-    el: '#app',
+    el: "#app",
     store,
     router,
     api,
     vuetify,
-    render: h => h(App),
+    render: (h) => h(App),
   });
-
 }
-
 
 main();
