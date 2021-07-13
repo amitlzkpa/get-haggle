9160d4eb8b89159607aeea42c9276beeff822ed6
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-12 22:09:52 -0600

    add square auth link to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -30,6 +30,9 @@
             <v-list-item to="/my-stores">
               <v-list-item-title> My Stores </v-list-item-title>
             </v-list-item>
+            <v-list-item to="/oauth-redirect">
+              <v-list-item-title> Square </v-list-item-title>
+            </v-list-item>
             <v-list-item to="/dashboard">
               <v-list-item-title> Dashboard </v-list-item-title>
             </v-list-item>
