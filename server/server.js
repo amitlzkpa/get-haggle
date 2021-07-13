07aff20743d00d0168b3dea362f60a9b9abec90b
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-13 03:19:51 -0600

    setup socket server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const DIR = "build";
 const PORT = process.env.PORT || 8080;
@@ -13,12 +15,19 @@ const mongoURI = process.env.MONGO_URL;
 mongoose.connect(mongoURI, { useNewUrlParser: true });
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(cors());
 app.use(express.static(DIR));
 
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
+
+io.on("connection", (socket) => {
+  console.log("a socket user connected");
+});
 
 app.use("/", require("./routes"));
 
