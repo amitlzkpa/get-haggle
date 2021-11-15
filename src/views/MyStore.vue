628801de77801f42fd9ed49e7ecc9b43ad32160c
amitlzkpa <amit.lzkpa@gmail.com>
2021-11-15 12:58:03 -0500

    tabs and cards for deals

--- src/views/MyStore.vue
+++ src/views/MyStore.vue
@@ -71,11 +71,101 @@
                 </div>
                 <v-spacer />
                 <div>
-                  <v-tabs right color="primary">
-                    <v-tab>Live</v-tab>
-                    <v-tab>Past</v-tab>
+                  <v-tabs v-model="activeTab" right color="primary">
+                    <v-tab href="#tab-live">Live</v-tab>
+                    <v-tab href="#tab-past">Past</v-tab>
                   </v-tabs>
                 </div>
+              </div>
+              <div>
+                <v-tabs-items v-model="activeTab">
+                  <v-tab-item value="tab-live">
+                    <v-card
+                      class="d-flex align-center pa-4"
+                      height="240"
+                      outlined
+                    >
+                      <div
+                        class="
+                          d-flex
+                          flex-column
+                          justify-center
+                          align-center
+                          px-12
+                        "
+                      >
+                        <v-progress-circular
+                          color="primary"
+                          size="160"
+                          width="4"
+                          rotate="270"
+                          :value="60"
+                        >
+                          <v-progress-circular
+                            color="secondary"
+                            size="200"
+                            width="8"
+                            rotate="270"
+                            :value="40"
+                          >
+                            <div
+                              class="
+                                d-flex
+                                flex-column
+                                justify-center
+                                align-center
+                              "
+                            >
+                              <v-tooltip bottom>
+                                <template v-slot:activator="{ on, attrs }">
+                                  <v-card
+                                    flat
+                                    class="d-flex align-end"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                  >
+                                    <h2
+                                      class="primary--text"
+                                      style="font-size: 2.2em"
+                                    >
+                                      34
+                                    </h2>
+                                    <h4 class="mb-2">/60</h4>
+                                  </v-card>
+                                </template>
+                                <span>34 people in current pool</span>
+                              </v-tooltip>
+                              <v-tooltip bottom>
+                                <template v-slot:activator="{ on, attrs }">
+                                  <v-card
+                                    flat
+                                    class="grey--text"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                  >
+                                    4/10
+                                  </v-card>
+                                </template>
+                                <span>4 pools filled</span>
+                              </v-tooltip>
+                            </div>
+                          </v-progress-circular>
+                        </v-progress-circular>
+                      </div>
+                      <div class="flex-grow-1">
+                        <div class="d-flex flex-column justify-center">
+                          <h2 class="primary--text">Thanksgiving Groups</h2>
+                          <p class="secondary--text">
+                            Coupon: 30% off
+                            <br />
+                            Ends: 1st December, 2021
+                          </p>
+                        </div>
+                      </div>
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item value="tab-past"> Past </v-tab-item>
+                </v-tabs-items>
               </div>
             </v-col>
 
@@ -88,8 +178,8 @@
                   <v-container fluid grid-list-md>
                     <v-layout row wrap>
                       <v-flex
-                        xs12
-                        md6
+                        lg12
+                        xl6
                         v-for="storeItem of storeInfo.storeItems"
                         :key="storeItem.id"
                       >
@@ -299,16 +389,16 @@ let storeInfoSample = {
 export default {
   data() {
     return {
+      activeTab: null,
       storeInfo: storeInfoSample
     };
   },
   async mounted() {
-    let postBody = {
-      storeDomain: this.$route.params.storeDomain
-    };
-    let res = await this.$api.post(`/api/square/get-store-details`, postBody);
-    console.log(res.data);
-    this.storeInfo = res.data;
+    // let postBody = {
+    //   storeDomain: this.$route.params.storeDomain
+    // };
+    // let res = await this.$api.post(`/api/square/get-store-details`, postBody);
+    // this.storeInfo = res.data;
   }
 };
 </script>
