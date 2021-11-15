8767cf0575923951dce38eaa18fd7c6ca681b492
amitlzkpa <amit.lzkpa@gmail.com>
2021-11-14 23:25:21 -0500

    send api ednpt in env vals

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,15 @@ const router = require("express").Router();
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 const SQUARE_CLIENT_ID = process.env.SQUARE_CLIENT_ID;
+const SQUARE_API_ENDPT = process.env.SQUARE_API_ENDPT;
 
 // these aren't really secrets
 router.get("/api/env", function(req, res) {
   const creds = {
-    AUTH0_DOMAIN: AUTH0_DOMAIN,
-    AUTH0_CLIENT_ID: AUTH0_CLIENT_ID,
-    SQUARE_CLIENT_ID: SQUARE_CLIENT_ID,
+    AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID,
+    SQUARE_CLIENT_ID,
+    SQUARE_API_ENDPT,
   };
   return res.json(creds);
 });
